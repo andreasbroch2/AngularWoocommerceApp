96bceac8ad533b5dcea20d02e686f0a2dc7427b2
andreasbroch2 <68319479+andreasbroch2@users.noreply.github.com>
2022-02-13 15:26:34 +0100

    push

---
+++ src/app/services/fcm.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from "@angular/core";
+import {     ActionPerformed,
+  PushNotificationSchema,
+  PushNotifications,
+  Token, } from "@capacitor/push-notifications";
+
+@Injectable({
+  providedIn: "root",
+})
+export class FcmService {
+    registerNotis(){
+      // Request permission to use push notifications
+      // iOS will prompt user and return if they granted permission or not
+      // Android will just grant without prompting
+      PushNotifications.requestPermissions().then(result => {
+        if (result.receive === 'granted') {
+          // Register with Apple / Google to receive push via APNS/FCM
+          PushNotifications.register();
+        } else {
+          // Show some error
+        }
+      });
+  
+      PushNotifications.addListener('registration', (token: Token) => {
+        alert('Push registration success, token: ' + token.value);
+      });
+  
+      PushNotifications.addListener('registrationError', (error: any) => {
+        alert('Error on registration: ' + JSON.stringify(error));
+      });
+  
+      PushNotifications.addListener(
+        'pushNotificationReceived',
+        (notification: PushNotificationSchema) => {
+          alert('Push received: ' + JSON.stringify(notification));
+        },
+      );
+  
+      PushNotifications.addListener(
+        'pushNotificationActionPerformed',
+        (notification: ActionPerformed) => {
+          alert('Push action performed: ' + JSON.stringify(notification));
+        },
+      );
+    }
+  }
